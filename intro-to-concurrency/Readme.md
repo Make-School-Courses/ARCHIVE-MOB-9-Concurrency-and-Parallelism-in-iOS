8908b59503bff48f8920324a8ffbebd2b6ec4eda
Eliel Gordon <gordoneliel@gmail.com>
2018-04-25 14:08:47 -0700

    add slides

--- intro-to-concurrency/Readme.md
+++ intro-to-concurrency/Readme.md
@@ -1,1 +1,16 @@
-# Intro to Concurrency 
+# Intro to Concurrency
+
+## Topics
+
+- Async
+- Sync
+- Concurrent
+- Parallelism
+- Task
+- Thread
+
+## Class Materials
+
+Slides:
+
+[Concurrency & Parallelism](assets/concurrency.key)
---
+++ intro-to-concurrency/assets/GCDPlay.playground/Pages/SameQueueAsync.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let queue = DispatchQueue(label: "com.makeschool.queue1", qos: .utility)
+
+queue.async {
+    for i in 0..<10 {
+        print("🍎 ", i)
+    }
+}
+queue.async {
+    for i in 100..<110 {
+        print("🐳 ", i)
+    }
+}
+
+queue.async {
+    for i in 1000..<1010 {
+        print("🍑 ", i)
+    }
+}
+
+//: [Next](@next)
